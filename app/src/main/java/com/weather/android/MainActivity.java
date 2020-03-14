101797cab593c837bf87e7e8c62e72316201f667
liupenglei <liupenglei@kuaishou.com>
2020-03-14 23:10:36 +0800

    first modified

--- app/src/main/java/com/weather/android/MainActivity.java
+++ app/src/main/java/com/weather/android/MainActivity.java
@@ -1,24 +1,102 @@
 package com.weather.android;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.weather.android.WeatherActivity;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.weather.android.service.LocationService;
+
+import static com.weather.android.util.Utility.checkLocationPermission;
+import static com.weather.android.util.Utility.isGPSOPen;
 
 public class MainActivity extends AppCompatActivity {
-
+  private LocationService locationService;
+  private static final int LOCATION_CODE = 1;
+  private String mProvince;
+  private String mCity;
+  private String mCounty;
+  private BDLocationListener mListener = new BDLocationListener() {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (prefs.getString("weather", null) != null) {
-            Intent intent = new Intent(this, WeatherActivity.class);
-            startActivity(intent);
-            finish();
-        }
+    public void onReceiveLocation(BDLocation location) {
+      mProvince = location.getProvince();
+      mCity = location.getCity();
+      mCounty = location.getDistrict();
+      Log.d("TAg", "onReceiveLocation: " + mProvince + mCity + mCounty);
+      if (!"".equals(mCounty) && locationService != null) {
+        locationService.stop();
+        Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+        intent.putExtra("provinceName", mProvince);
+        intent.putExtra("cityName", mCity);
+        intent.putExtra("countyName", mCounty);
+        startActivity(intent);
+        finish();
+      } else {
+        Toast.makeText(MainActivity.this, "定位失败！", Toast.LENGTH_SHORT).show();
+      }
     }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    locationService = new LocationService(getApplicationContext());
+    locationService.registerListener(mListener);
+    //注册监听
+    locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    if (prefs.getString("weather", null) != null) {
+      Intent intent = new Intent(this, WeatherActivity.class);
+      startActivity(intent);
+      finish();
+    } else {
+      if (!checkLocationPermission(this)) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_CODE);
+      }
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 1315) {
+      if (isGPSOPen(this)) {
+        locationService.start();
+      } else {
+        Toast.makeText(this, "GPS未开启！请手动选择城市", Toast.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    switch (requestCode) {
+      case LOCATION_CODE: {
+        if (grantResults.length > 0
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          if (isGPSOPen(this)) {
+            locationService.start();
+          } else {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 1315);
+          }
+        } else {
+          // 权限被用户拒绝了。
+          Toast.makeText(MainActivity.this, "定位权限被禁止，相关地图功能无法使用！", Toast.LENGTH_LONG).show();
+        }
+      }
+    }
+  }
 }
--- app/src/main/java/com/weather/android/util/Utility.java
+++ app/src/main/java/com/weather/android/util/Utility.java
@@ -1,7 +1,13 @@
 package com.weather.android.util;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
-
 import com.weather.android.db.City;
 import com.weather.android.db.County;
 import com.weather.android.db.Province;
@@ -12,89 +18,123 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static android.app.PendingIntent.getActivity;
+import static org.litepal.LitePalApplication.getContext;
+
 public class Utility {
-
-    /**
-     * 解析和处理服务器返回的省级数据
-     */
-    public static boolean handleProvinceResponse(String response) {
-        if (!TextUtils.isEmpty(response)) {
-            try {
-                JSONArray allProvinces = new JSONArray(response);
-                for (int i = 0; i < allProvinces.length(); i++) {
-                    JSONObject provinceObject = allProvinces.getJSONObject(i);
-                    Province province = new Province();
-                    province.setProvinceName(provinceObject.getString("name"));
-                    province.setProvinceCode(provinceObject.getInt("id"));
-                    province.save();
-                }
-                return true;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+  private static ProgressDialog progressDialog;
+  /**
+   * 解析和处理服务器返回的省级数据
+   */
+  public static boolean handleProvinceResponse(String response) {
+    if (!TextUtils.isEmpty(response)) {
+      try {
+        JSONArray allProvinces = new JSONArray(response);
+        for (int i = 0; i < allProvinces.length(); i++) {
+          JSONObject provinceObject = allProvinces.getJSONObject(i);
+          Province province = new Province();
+          province.setProvinceName(provinceObject.getString("name"));
+          province.setProvinceCode(provinceObject.getInt("id"));
+          province.save();
         }
-        return false;
+        return true;
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
     }
+    return false;
+  }
 
-    /**
-     * 解析和处理服务器返回的市级数据
-     */
-    public static boolean handleCityResponse(String response, int provinceId) {
-        if (!TextUtils.isEmpty(response)) {
-            try {
-                JSONArray allCities = new JSONArray(response);
-                for (int i = 0; i < allCities.length(); i++) {
-                    JSONObject cityObject = allCities.getJSONObject(i);
-                    City city = new City();
-                    city.setCityName(cityObject.getString("name"));
-                    city.setCityCode(cityObject.getInt("id"));
-                    city.setProvinceId(provinceId);
-                    city.save();
-                }
-                return true;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+  /**
+   * 解析和处理服务器返回的市级数据
+   */
+  public static boolean handleCityResponse(String response, int provinceId) {
+    if (!TextUtils.isEmpty(response)) {
+      try {
+        JSONArray allCities = new JSONArray(response);
+        for (int i = 0; i < allCities.length(); i++) {
+          JSONObject cityObject = allCities.getJSONObject(i);
+          City city = new City();
+          city.setCityName(cityObject.getString("name"));
+          city.setCityCode(cityObject.getInt("id"));
+          city.setProvinceId(provinceId);
+          city.save();
         }
-        return false;
+        return true;
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
     }
+    return false;
+  }
 
-    /**
-     * 解析和处理服务器返回的县级数据
-     */
-    public static boolean handleCountyResponse(String response, int cityId) {
-        if (!TextUtils.isEmpty(response)) {
-            try {
-                JSONArray allCounties = new JSONArray(response);
-                for (int i = 0; i < allCounties.length(); i++) {
-                    JSONObject countyObject = allCounties.getJSONObject(i);
-                    County county = new County();
-                    county.setCountyName(countyObject.getString("name"));
-                    county.setWeatherId(countyObject.getString("weather_id"));
-                    county.setCityId(cityId);
-                    county.save();
-                }
-                return true;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+  /**
+   * 解析和处理服务器返回的县级数据
+   */
+  public static boolean handleCountyResponse(String response, int cityId) {
+    if (!TextUtils.isEmpty(response)) {
+      try {
+        JSONArray allCounties = new JSONArray(response);
+        for (int i = 0; i < allCounties.length(); i++) {
+          JSONObject countyObject = allCounties.getJSONObject(i);
+          County county = new County();
+          county.setCountyName(countyObject.getString("name"));
+          county.setWeatherId(countyObject.getString("weather_id"));
+          county.setCityId(cityId);
+          county.save();
         }
-        return false;
+        return true;
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
     }
+    return false;
+  }
 
-    /**
-     * 将返回的JSON数据解析成Weather实体类
-     */
-    public static Weather handleWeatherResponse(String response) {
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
-            String weatherContent = jsonArray.getJSONObject(0).toString();
-            return new Gson().fromJson(weatherContent, Weather.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+  /**
+   * 将返回的JSON数据解析成Weather实体类
+   */
+  public static Weather handleWeatherResponse(String response) {
+    try {
+      JSONObject jsonObject = new JSONObject(response);
+      JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+      String weatherContent = jsonArray.getJSONObject(0).toString();
+      return new Gson().fromJson(weatherContent, Weather.class);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return null;
+  }
+
+  /**
+   * 检查获取位置权限
+   */
+  public static boolean checkLocationPermission(Activity activity) {
+    if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * 判断GPS是否开启，GPS或者AGPS开启一个就认为是开启的
+   *
+   * @param
+   * @return true 表示开启
+   */
+  public static final boolean isGPSOPen(final Context context) {
+    LocationManager locationManager
+        = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    // 通过GPS卫星定位，定位级别可以精确到街（通过24颗卫星定位，在室外和空旷的地方定位准确、速度快）
+    boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    // 通过WLAN或移动网络(3G/2G)确定的位置（也称作AGPS，辅助GPS定位。主要用于在室内或遮盖物（建筑群或茂密的深林等）密集的地方定位）
+    boolean network = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    if (gps || network) {
+      return true;
+    }
+    return false;
+  }
 
 }
